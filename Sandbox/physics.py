25626d0c49537bae0e354dd9747375c41266b827
Jeffrey Clyne <Jeff@Jeffreys-MacBook-Pro.local>
2013-05-15 22:38:35 -0700

    a couple of random physics methods

---
+++ Sandbox/physics.py
@@ -0,0 +1,15 @@
+def isOnGround(character, blocks):
+	for block in blocks:
+		if character.bottom != block.bottom: continue
+
+		if character.left > block.left and character.left < block.right:
+			return true
+		elif character.right > block.left and character.right > block.right:
+			return true
+
+	return false
+
+def addGravity(characters):
+	for character in characters
+		if !isOnGround(character, blocks):
+			character.ySpeed += gravity
