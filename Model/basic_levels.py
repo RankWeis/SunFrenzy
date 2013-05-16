359804aaea1a1727faeb6991fa86f75fd58b15c5
Kian Samii <kian.samii@gmail.com>
2013-05-16 13:38:49 -0700

    Moving imagefile to view Updating level1 Changing 'while 1' to 'while True' (only because I don't know what revert is in git) adding a couple of resource folders

---
+++ Model/basic_levels.py
@@ -0,0 +1,24 @@
+"""
+Eventually need to create some sort of level editor.
+For simplicity, array string of nxm size can be used.
+W = wall
+G = ground (could be wall I guess)
+B = Block, could be more descriptive later
+I = invisible block, somehow need to feed in conditions, but for now can be on all enemies killed
+E = Enemy
+S = Starting Position
+
+First level is not side scrolling, to get things up and running.
+"""
+
+level1 = [
+"WWWWWWWWWWWWWWWWWWWW",
+"W                    W",
+"W                    W",
+"W F   E              W"
+"W  BBBBB   II   BB   W"
+"W                  BBW"
+"W                    W"
+"W  S                 W"
+"WGGGGGG GGGG    GGGW"
+]
